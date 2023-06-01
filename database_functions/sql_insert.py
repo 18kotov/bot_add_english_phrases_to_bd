0c2ee4aeb42f7ac918fa6756808c9549214e720f
18kotov <twozerocool@gmail.com>
2023-06-01 12:17:56 +0300

    Update sql_insert.py
    исправил SQL запрос

--- database_functions/sql_insert.py
+++ database_functions/sql_insert.py
@@ -1,12 +1,11 @@
 from settings import get_logger
-from datetime import datetime
 from database_functions.commucation_with_db import post_query
 
 logger = get_logger(__name__)
 
 @post_query
-def add_phrases(english: str, russian: str, date=str(datetime.today().date())):
-    query = f"INSERT INTO phrases (english, russian, date) VALUES('{english}','{russian}', '{date}');"
+def add_phrases(english: str, russian: str):
+    query = f"INSERT INTO phrases (english, russian) VALUES('{english}','{russian}');"
     return query
 
 
