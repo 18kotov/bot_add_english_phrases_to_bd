703406dde6da2f6408d646a4c6ed3ca0b002ebf5
18kotov <twozerocool@gmail.com>
2023-06-09 12:31:11 +0300

    вынес название таблицы в SQL запросах в переменную окружения

--- database_functions/sql_edit.py
+++ database_functions/sql_edit.py
@@ -1,3 +1,4 @@
+import os
 from settings import get_logger
 from database_functions.commucation_with_db import post_query
 
@@ -7,6 +8,7 @@ logger = get_logger(__name__)
 
 @post_query
 def delete_phrase(english: str)->str:
-    query = f"DELETE FROM phrases WHERE english = '{english}';"
+    table = os.environ.get('TABLE')
+    query = f"DELETE FROM {table} WHERE english = '{english}';"
     return query
 
